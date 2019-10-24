fa149c094197d8a64ba22035fa68c2e1b2d25d1e
Yash <yashrastogi@gmail.com>
2019-10-25 02:26:15 +0530

    Fix crash without redux devtools extension

--- client/src/store.js
+++ client/src/store.js
@@ -24,7 +24,7 @@ const store = createStore(
   compose(
     applyMiddleware(...middleware),
     // Comment below if running without redux devtools extension
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
