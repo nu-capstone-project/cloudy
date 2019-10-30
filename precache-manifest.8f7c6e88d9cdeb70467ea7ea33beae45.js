5d65b103f7e4ce596bd648858848610e29bf0590
Yash <yashrastogi@gmail.com>
2019-10-30 15:07:08 +0530

    Updates

---
+++ precache-manifest.8f7c6e88d9cdeb70467ea7ea33beae45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf80f58642205a75e295ee8af7035b7",
+    "url": "/cloudy/index.html"
+  },
+  {
+    "revision": "7ca118454d35b8587dce",
+    "url": "/cloudy/static/css/main.8264eba4.chunk.css"
+  },
+  {
+    "revision": "8f404f9108cd5dae7b3f",
+    "url": "/cloudy/static/js/2.bc51de82.chunk.js"
+  },
+  {
+    "revision": "7ca118454d35b8587dce",
+    "url": "/cloudy/static/js/main.d0b653d7.chunk.js"
+  },
+  {
+    "revision": "02ba5ad1cf9150f114f9",
+    "url": "/cloudy/static/js/runtime-main.5c1b3a31.js"
+  }
+]);
